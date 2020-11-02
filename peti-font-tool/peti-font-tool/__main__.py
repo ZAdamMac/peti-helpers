89c46118d0b3ee20fff40f8a3e7f782d1b8d08b4
Zac Adam-MacEwen <zadammac@kenshosec.com>
2020-11-02 18:35:00 -0400

    Initial commit

---
+++ peti-font-tool/peti-font-tool/__main__.py
@@ -0,0 +1,12 @@
+"""The PETI Font Tool is a specialized editor for manipulating C Header Files
+which describe fonts in the way PETI would expect to encounter them. For
+complete usage information, refer to the readme.md in this package, or at the
+main development repo:
+
+github: https://www.github.com/ZAdamMac/peti-helpers
+"""
+
+from . import functions as tapestry
+
+if __name__ == "__main__":
+    tapestry.runtime()
